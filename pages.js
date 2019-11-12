f0683a8f07d010ff66bf27a4c0e47b37647473c6
DOODpls <floyd_santos64@yahoo.com>
2019-11-12 06:02:36 -0700

    added header partials

--- pages.js
+++ pages.js
@@ -1,23 +1,28 @@
 const pagesInfo = {
   index: {
-    title: "Login"
+    title: "Login",
+    csslk: "css/index_reg.css"
   },
   profile: {
     title: "Your Profile"
   },
   registered: {
-    title: "Registered"
+    title: "Registered",
+    csslk: "css/registered.css"
   },
   alreadysub:{
-    title: "Account Already Exist"
+    title: "Account Already Exist",
+    csslk: "css/alreadsub.css"
   },
   notfound:{
     title: "404 NOT FOUND",
-    message: "404 NOT FOUND"
+    message: "404 NOT FOUND",
+    csslk: "css/notfound.css"
   },
   error: {
     title: "Error: Something Happend",
-    message: "Something Happened"
+    message: "Something Happened",
+    csslk: "css/notfound.css"
   }
 }
 
